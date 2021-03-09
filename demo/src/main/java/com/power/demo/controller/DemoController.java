ca2dd0f2847f2cb213aae6b6fbf637168c1c5821
wangll <w1109589373@163.com>
2021-03-09 21:18:32 +0800

    测试依赖

---
+++ demo/src/main/java/com/power/demo/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.power.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "hello, wang sir!";
+    }
+
+
+}
